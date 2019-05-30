8c23c62187cbc696b64e434cd915b642e6e1ac20
wudaopeng <444257580@qq.com>
2019-05-30 14:07:48 +1000

    final

--- appIE.R
+++ appIE.R
@@ -1,10 +1,10 @@
 library(shiny)
 library(datasets)
-library(magrittr) # need to run every time you start R and want to use %>%
-library(dplyr)    # alternative, this also loads %>%
+library(magrittr) 
+library(dplyr)    
 library(ggplot2)
 library(shinyWidgets)
-dementiaData = read.csv("State-Territory-Dementia.csv",header = T)
+
 cost = read.csv("costComparison.csv",header=T)
 
 ui <- fluidPage(setBackgroundColor("#222222"),
@@ -27,7 +27,6 @@ ui <- fluidPage(setBackgroundColor("#222222"),
                  
                  radioButtons("rbreason",  HTML("<h3 style='color:white;'>Reasons for hospital care:</h3>"),
                               choiceNames = list(
-                                
                                 HTML("<p style='color:white;'>Admit for Renal Dialysis</p>"),
                                 HTML("<p style='color:white;'>Rehabilitation</p>"),
                                 HTML("<p style='color:white;'>Dementia and Other Chronic Disturbances of Cerebral Function</p>"),
@@ -36,25 +35,19 @@ ui <- fluidPage(setBackgroundColor("#222222"),
                                 HTML("<p style='color:white;'>Kidney and Urinary Tract Infections</p>"),
                                 HTML("<p style='color:white;'>Injuries</p>"),
                                 HTML("<p style='color:white;'>Heart Failure and Shock</p>"),
-                                HTML("<p style='color:white;'>Oesophagitis, Gastroenteritis and Misc Digestive System Disorders Age >9</p>"), 
+                                HTML("<p style='color:white;';label title='The reflux of stomach contents into the oesophagus (tube leading to the stomach)'>Oesophagitis, Gastroenteritis and Misc Digestive System Disorders Age >9</p>"), 
                                 HTML("<p style='color:white;'>Stroke</p>"),
                                 HTML("<p style='color:white;'>Other hip and femur procedures</p>"),
                                 HTML("<p style='color:white;'> Chronic obstructive airways disease</p>"),
                                 HTML("<p style='color:white;'>Syncope and collapse</p>"),
-                                HTML("<p style='color:white;'>Delirium</p>"),
-                                
-                                HTML("<p style='color:white;'>Septicaemia</p>"),
+                                HTML("<p style='color:white;';label title='Serious disturbance in mental abilities that results in confused thinking and reduced awareness of surroundings.'>Delirium</p>"),
+                                HTML("<p style='color:white;';label title='Septicaemia is an infection caused by large amounts of bacteria entering the bloodstream.'>Septicaemia</p>"), 
                                 HTML("<p style='color:white;'>Chest pain</p>"), 
                                 HTML("<p style='color:white;'>Other disorders of the nervous system</p>"),
                                 HTML("<p style='color:white;'> Hip revision or replacement</p>"),
                                 HTML("<p style='color:white;'> Non-surgical spinal disorders</p>"),
-                                HTML("<p style='color:white;'>Cellulitis</p>"),
+                                HTML("<p style='color:white;';label title='Cellulitis is a bacterial infection of the skin that tends to occur on the lower legs and in areas that are damaged or inflamed'>Cellulitis</p>"), 
                                 HTML("<p style='color:white;'>All</p>")
-                                
-                                
-                                #img(id="img9",src = "English.png", height = 40, width=80),
-                                #img(id="img10",src = "Italian.png", height = 40, width=80),
-                                #img(id="img11",src = "Chinese.png", height = 40, width=80),
                               ),
                               choiceValues = list("Admit for Renal Dialysis","Rehabilitation","Dementia and Other Chronic Disturbances of Cerebral Function",
                                                   "Respiratory Infections/Inflammations","Other Factors Influencing Health Status",
@@ -69,9 +62,6 @@ ui <- fluidPage(setBackgroundColor("#222222"),
     ),
     
     mainPanel(
-      
-    
-      
       br(),
       HTML("<h1 style='color:white'; align='center'>Average hospital care costs comparison between people with and without dementia</h1>"),
       br(),
@@ -79,112 +69,50 @@ ui <- fluidPage(setBackgroundColor("#222222"),
       br(),
       textOutput('text1'),
       tags$head(tags$style("#text1{color: #ADD8E6;
-                                 font-size: 30px;
-                                 font-style: italic;  
-                                 text-align: center
-
-
-                                 }"
+                           font-size: 30px;
+                           font-style: italic;  
+                           text-align: center
+                           }"
       )
       ),
-      
-  
       
       br(),
       br(),
       
       plotOutput("costplot")
-      
-      
-      
-      
+
+      )
+      )
     )
-  )
-  
-  
-  
-  
-  
-  #headerPanel("Average costs comparison between people with and without dementia"), 
-  
-  
-  
-  
-                )
 
 server <- function(input, output) {
-  #output$placeholder <- renderText({ input$rbreason })
   
-  output$text1 <- renderText({ paste(input$rbreason) })
-  
-  output$coolplot <- renderPlot(
-    
-    
-    
-    {
-      if(input$rbState == "Show all states")
-      {
-        ggplot(dementiaData,aes(Year,Number,colour=State)) + 
-          geom_line() + geom_text(aes(label = Number, y = Number,fontface = "bold"),check_overlap = TRUE,size=4,color="Black")
-      }
-      else
-      {
-        filtered <-
-          dementiaData %>%
-          filter(
-            State == input$rbState
-            
-          )
-        
-        
-        ggplot(filtered,aes(Year,Number,label=Number)) + 
-          geom_point(aes(color=State)) + facet_wrap(~State) + geom_smooth(method = "lm", formula = y ~ x, size = 1) +  geom_text(aes(label = Number, y = Number,fontface = "bold"), ,check_overlap = TRUE,size=4)
-        
-        
-        
-      }})
-  
-  
-  
+  output$text1 <- renderText({ paste(input$rbreason) }) 
   output$costplot <- renderPlot({
+    
+    #Filter the data based on the radio button selection.
     
     if(input$rbreason == "Show all reasons")
     {
       ggplot(data=cost, aes(x=Reason, y=cost, fill=status,label=cost)) +
         geom_bar(stat="identity", position=position_dodge(), size = 1) +   geom_text(aes(label = cost, y = cost,fontface = "bold"),check_overlap = TRUE,size=4)  +
         coord_flip()  
-      
     }
     else
     {
-      
-      
-      
       filteredcost <-
         cost %>%
         filter(
           Reason== input$rbreason
-          
         )
-      
-      
       ggplot(data=filteredcost, aes(x=Reason, y=cost, fill=status,label=cost)) +
         geom_bar(stat="identity", position=position_dodge(), size = 1) +   geom_text(aes(label = cost, y = cost,fontface = "bold"),check_overlap = TRUE,size=5, position=position_dodge(width=0.9), vjust=-0.25)  
-      
-      
-      
-      
-      
-      
-      
     }})
-  
-  
-  
 }
 
 
 shinyApp(ui = ui, server = server)
+
 
 
 
